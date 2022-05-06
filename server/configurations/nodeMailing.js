92cbe51b396bbdfde1edb2737396d40bfec90859
Walid Guirat <68691125+GuiratWalid@users.noreply.github.com>
2022-05-06 12:30:38 +0100

    Update nodeMailing.js

--- server/configurations/nodeMailing.js
+++ server/configurations/nodeMailing.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
     port: 465,               // true for 465, false for other ports
     host: "smtp.gmail.com",
     auth: {
-        user: 'guiratguirat123@gmail.com',
-        pass: 'guiratguirat123',
+        user: '@gmail.com',
+        pass: '',
     },
     secure: true,
 });
